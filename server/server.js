683553222a5941605bd7cc8062610cbdeb47e4e1
Andrew Akagawa <andrew@Mevisahp>
2023-06-27 23:02:23 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.post('/api', (req, res) => {
+  console.log(req.body)
+  res.status(200).json({ result: req.body.text });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
